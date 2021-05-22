0df19ea82509bc66355288c947d8264ce29914ef
Jacob Enriquez <jyenriquez@gmail.com>
2021-05-22 03:46:30 -0400

    Added rough login functionality

--- main.py
+++ main.py
@@ -3,33 +3,60 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from mongo import post_name
+from flask import Flask, session, redirect, url_for, escape, request
 
-# Create a Flask Instance
+# Creates a Flask Instance
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "my super secret key"
+# app.config['SECRET_KEY'] = "my super secret key"
+app.secret_key = 'a random string'
 
-# Create a Form Class
+# Creates a Form Class
 class NamerForm(FlaskForm):
     name = StringField("What's Your Name", validators = [DataRequired()])
     submit = SubmitField("Submit")
 
+# Index Page
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if session.get('user') is None:
+        return render_template("index.html", logged_in = False)
+    return render_template("index.html", logged_in = True, user = session.get('user'))
 
+# Logout page
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('user', None)
+   return redirect(url_for('index'))
+
+# Username page
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
 
+# Page that shows a form
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
     form = NamerForm()
+    already_submitted = False
+
     # Validate Form
     if form.validate_on_submit():
         name = form.name.data
         post_name(name)
+        session['user'] = name
         form.name.data = ''
 
-    return render_template("name.html", name=name, form=form)
+    if session.get('user') is not None:
+        already_submitted = True
 
+    return render_template("name.html", name=name, form=form, 
+    already_submitted = already_submitted, user = session.get('user'))
+
+# TODO: Create a working message board
+
+# Message page
+@app.route('/messages')
+def message():
+    return render_template("messages.html")
--- mongo.py
+++ mongo.py
@@ -10,8 +10,18 @@ cluster = MongoClient("mongodb+srv://poporing:7sv7T7Vt0cPt@cluster0.tliy9.mongod
 db = cluster["test"]
 collection = db["test"]
 
+# Posts a new name to the DB
 def post_name(name):
+    if get_name(name) is True:
+        return
     post = {"name": name, "dateAdded": datetime.today()}
     collection.insert_one(post)
+
+# Gets user only by the name
+def get_name(name):
+    found_name = collection.find_one({"name": name})
+    if found_name is None:
+        return False
+    return True
 
 
